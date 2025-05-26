fb59685bd9371c10dea33d7d93d37fdd997f76f2
WitheringAway <124115470+notwithering@users.noreply.github.com>
2025-05-25 20:35:22 -0400

    fix `KongSignal` `Decode` function

--- signal.go
+++ signal.go
@@ -12,93 +12,95 @@ import (
 type KongSignal syscall.Signal
 
 func (s *KongSignal) Decode(ctx *kong.DecodeContext) error {
-	var raw string
-	if err := ctx.Scan.PopValueInto("signal", &raw); err != nil {
+	var value string
+	err := ctx.Scan.PopValueInto("signal", &value)
+	if err != nil {
 		return err
 	}
 
-	var errUnknownSignal = fmt.Errorf("unknown signal \"%s\"", raw)
+	var errUnknownSignal = fmt.Errorf("unknown signal %s", value)
 
-	n, err := strconv.Atoi(raw)
+	n, err := strconv.ParseInt(value, 10, 0)
 	if err == nil {
-		if *s < 0 || *s > 31 {
+		if n <= 0 || n > 31 {
 			return errUnknownSignal
 		}
 		*s = KongSignal(n)
 		return nil
 	}
 
-	switch strings.ToUpper(raw) {
+	// Formatted from builtin/syscall/zerrors_linux_amd64.go "Signals" const block.
+	switch strings.TrimPrefix(strings.ToUpper(value), "SIG") {
 	case "ABRT":
-		*s = KongSignal(0x6)
+		*s = 0x6
 	case "ALRM":
-		*s = KongSignal(0xe)
+		*s = 0xe
 	case "BUS":
-		*s = KongSignal(0x7)
+		*s = 0x7
 	case "CHLD":
-		*s = KongSignal(0x11)
+		*s = 0x11
 	case "CLD":
-		*s = KongSignal(0x11)
+		*s = 0x11
 	case "CONT":
-		*s = KongSignal(0x12)
+		*s = 0x12
 	case "FPE":
-		*s = KongSignal(0x8)
+		*s = 0x8
 	case "HUP":
-		*s = KongSignal(0x1)
+		*s = 0x1
 	case "ILL":
-		*s = KongSignal(0x4)
+		*s = 0x4
 	case "INT":
-		*s = KongSignal(0x2)
+		*s = 0x2
 	case "IO":
-		*s = KongSignal(0x1d)
+		*s = 0x1d
 	case "IOT":
-		*s = KongSignal(0x6)
+		*s = 0x6
 	case "KILL":
-		*s = KongSignal(0x9)
+		*s = 0x9
 	case "PIPE":
-		*s = KongSignal(0xd)
+		*s = 0xd
 	case "POLL":
-		*s = KongSignal(0x1d)
+		*s = 0x1d
 	case "PROF":
-		*s = KongSignal(0x1b)
+		*s = 0x1b
 	case "PWR":
-		*s = KongSignal(0x1e)
+		*s = 0x1e
 	case "QUIT":
-		*s = KongSignal(0x3)
+		*s = 0x3
 	case "SEGV":
-		*s = KongSignal(0xb)
+		*s = 0xb
 	case "STKFLT":
-		*s = KongSignal(0x10)
+		*s = 0x10
 	case "STOP":
-		*s = KongSignal(0x13)
+		*s = 0x13
 	case "SYS":
-		*s = KongSignal(0x1f)
+		*s = 0x1f
 	case "TERM":
-		*s = KongSignal(0xf)
+		*s = 0xf
 	case "TRAP":
-		*s = KongSignal(0x5)
+		*s = 0x5
 	case "TSTP":
-		*s = KongSignal(0x14)
+		*s = 0x14
 	case "TTIN":
-		*s = KongSignal(0x15)
+		*s = 0x15
 	case "TTOU":
-		*s = KongSignal(0x16)
+		*s = 0x16
 	case "UNUSED":
-		*s = KongSignal(0x1f)
+		*s = 0x1f
 	case "URG":
-		*s = KongSignal(0x17)
+		*s = 0x17
 	case "USR1":
-		*s = KongSignal(0xa)
+		*s = 0xa
 	case "USR2":
-		*s = KongSignal(0xc)
+		*s = 0xc
 	case "VTALRM":
-		*s = KongSignal(0x1a)
+		*s = 0x1a
 	case "WINCH":
-		*s = KongSignal(0x1c)
+		*s = 0x1c
 	case "XCPU":
-		*s = KongSignal(0x18)
+		*s = 0x18
 	case "XFSZ":
-		*s = KongSignal(0x19)
+		*s = 0x19
 	default:
 		return errUnknownSignal
 	}
